44eae1bdc96ba209e33cc0f9e8a5e3afc838ed69
howard <jshoward@itcom888.com>
2019-09-27 11:39:27 +0800

    Spring Boot数据持久化之JdbcTemplate

--- jdbctemplate/src/main/java/org/geekjc/jdbctemplate/Book.java
+++ jdbctemplate/src/main/java/org/geekjc/jdbctemplate/Book.java
@@ -5,4 +5,40 @@ package org.geekjc.jdbctemplate;
  * @date 2019年09月27日 11:32 AM
  */
 public class Book {
+    private Long id;
+    private String address;
+    private String username;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", address='" + address + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
