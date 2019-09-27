7d36b2b5427f2e755653fc78f77549e88e637464
howard <jshoward@itcom888.com>
2019-09-27 17:39:36 +0800

    Spring Boot整合Jpa多数据源

--- jpamulti/src/main/java/org/geekjc/jpamulti/dao/BookDao.java
+++ jpamulti/src/main/java/org/geekjc/jpamulti/dao/BookDao.java
@@ -1,8 +1,27 @@
 package org.geekjc.jpamulti.dao;
 
+import org.geekjc.jpamulti.bean.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 /**
  * @author ll
  * @date 2019年09月27日 5:13 PM
  */
-public class BookDao {
+public interface BookDao extends JpaRepository<Book,Long> {
+    List<Book> findBookByIdGreaterThanEqual(Long id);
+
+    List<Book> findBookByIdGreaterThanEqualAndAuthorStartingWith(Long id, String author);
+
+    @Query(value = "select count(*) from t_book",nativeQuery = true)
+    Long totalCount();
+
+    //    @Query(value = "update t_book set author=?1 where book_name=?2",nativeQuery = true)
+    @Query(value = "update t_book set author=:author where book_name=:name",nativeQuery = true)
+    @Modifying
+    Integer updateBookByName(@Param("author") String author, @Param("name") String name);
 }
--- jpamulti/src/main/java/org/geekjc/jpamulti/service/BookService.java
+++ jpamulti/src/main/java/org/geekjc/jpamulti/service/BookService.java
@@ -9,5 +9,12 @@ import org.springframework.transaction.annotation.Transactional;
  * @author ll
  * @date 2019年09月27日 4:57 PM
  */
+@Service
+@Transactional
 public class BookService {
+    @Autowired
+    BookDao bookDao;
+    public Integer updateBookByName(String author, String name) {
+        return bookDao.updateBookByName(author, name);
+    }
 }
