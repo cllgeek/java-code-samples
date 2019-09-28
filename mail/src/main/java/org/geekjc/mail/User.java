84598a9f72cea415e1223b030adccb4418cbf2b2
howard <jshoward@itcom888.com>
2019-09-28 17:15:15 +0800

    Spring Boot 邮件发送的 5 种姿势

---
+++ mail/src/main/java/org/geekjc/mail/User.java
@@ -0,0 +1,35 @@
+package org.geekjc.mail;
+
+/**
+ * @author ll
+ * @date 2019年09月28日 4:54 PM
+ */
+public class User {
+    private String username;
+    private Integer num;
+    private Double salary;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
